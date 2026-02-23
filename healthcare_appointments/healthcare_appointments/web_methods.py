4555a8db07fd394cf918bd97a666d7bbc9734be3
neeraj9956 <neeraj880834@gmail.com>
2026-02-24 01:45:27 +0530

    Added total_amount and some code optimization

--- healthcare_appointments/healthcare_appointments/web_methods.py
+++ healthcare_appointments/healthcare_appointments/web_methods.py
@@ -7,27 +7,12 @@ from erpnext.accounts.doctype.payment_entry.payment_entry import get_payment_ent
 def book_appointment(data):
 
     data = frappe.parse_json(data)
-
-    # 🔹 Get Service Doc (IMPORTANT)
     service_doc = frappe.get_doc("Healthcare Service", data.get("service"))
-
     duration = service_doc.duration_minutes
     price = service_doc.price
-
-    # 🔹 Convert date + time into datetime
-    start_datetime = get_datetime(
-        f"{data.get('appointment_date')} {data.get('appointment_time')}"
-    )
-
-    # 🔹 Calculate End Time
-    end_datetime = add_to_date(
-        start_datetime,
-        minutes=duration
-    )
-
+    start_datetime = get_datetime(f"{data.get('appointment_date')} {data.get('appointment_time')}")
+    end_datetime = add_to_date(start_datetime,minutes=duration)
     end_time = end_datetime.time()
-
-    # 🔹 Create Appointment
     appointment = frappe.get_doc({
         "doctype": "Patient Appointments",
         "patient_name": data.get("patient_name"),
@@ -36,16 +21,13 @@ def book_appointment(data):
         "appointment_time": data.get("appointment_time"),
         "service": data.get("service"),
         "gender": data.get("gender"),
-        "end_time": end_time
+        "end_time": end_time,
+        "total_amount":price
     })
-
     appointment.insert(ignore_permissions=True)
-
     si = frappe.new_doc("Sales Invoice")
     si.customer = data.get("patient_name")
     si.posting_date = today()
-    si.company = "Healthcare"
-
     si.append("items", {
         "item_code": service_doc.name,
         "qty": 1,
@@ -56,7 +38,6 @@ def book_appointment(data):
     si.submit()
     pe = get_payment_entry("Sales Invoice", si.name)
     pe.mode_of_payment = "Cash"
-    pe.company = "Healthcare"
     pe.insert(ignore_permissions=True)
     pe.submit()
 
